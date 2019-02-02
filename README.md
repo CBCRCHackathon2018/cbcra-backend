bcd5e431888f8d564271c81f7863e60d43a203c2
Abid Rahman <arahman1@ualberta.ca>
2019-02-02 15:49:27 -0700

    Add sample show data

---
+++ README.md
@@ -0,0 +1,8 @@
+This repository hosts the backend for the CBCRC-Hackthon submission for Team PaypalGang
+
+
+Install flask with:
+`pip install Flask`
+
+Run with:
+`FLASK_APP=api.py flask run`
--- api.py
+++ api.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask import jsonify
+import json
+from pprint import pprint
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Meg you silly boi"
+@app.route('/')
+def index():
+    return 'This API is for the CBCRAHackathon - Team PaypalGang'
+
+@app.route("/shows/<userId>")
+def get_shows(userId):
+    with open('show_data.json') as f:
+        data = json.load(f)
+    return jsonify(data);
+
